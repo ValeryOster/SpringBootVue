091cad8b77c377fd33170cd56f517f7c51fda493
valeryoster <oster_valera@hotmail.com>
2025-04-22 13:47:34 +0200

    Initial commit

---
+++ src/test/java/de/nexus/springbootvue/SpringBootVueApplicationTests.java
@@ -0,0 +1,13 @@
+package de.nexus.springbootvue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
