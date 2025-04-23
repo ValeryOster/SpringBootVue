aee21098e5d21f75b083df55faf84d161910c147
valeryoster <oster_valera@hotmail.com>
2025-04-23 23:45:34 +0200

    mysql, test dates for spring security

---
+++ src/main/java/de/nexus/springbootvue/config/WebAuthorizationConfig.java
@@ -0,0 +1,19 @@
+package de.nexus.springbootvue.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+public class WebAuthorizationConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/hello").permitAll()
+                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                .anyRequest().permitAll());
+        return http.build();
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO `nexusdb`.`authorities` VALUES (1, 'arr', 'write');
+INSERT INTO `nexusdb`.`users` VALUES (1, 'arr', '777', '1');
