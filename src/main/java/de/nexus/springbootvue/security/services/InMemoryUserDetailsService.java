870a5d4b723fd04fccaec675f0c6f502b19408cb
valeryoster <oster_valera@hotmail.com>
2025-04-27 19:42:35 +0200

    spring security

--- src/main/java/de/nexus/springbootvue/security/services/InMemoryUserDetailsService.java
+++ src/main/java/de/nexus/springbootvue/security/services/InMemoryUserDetailsService.java
@@ -1,10 +1,9 @@
-package de.nexus.springbootvue.services;
+package de.nexus.springbootvue.security.services;
 
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class InMemoryUserDetailsService implements UserDetailsService {
