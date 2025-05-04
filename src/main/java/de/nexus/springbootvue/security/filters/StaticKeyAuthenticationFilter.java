e3ceb18566b75cd090d7e4c9ab9e2ea4b8977911
valeryoster <oster_valera@hotmail.com>
2025-05-04 19:48:21 +0200

    spring security

---
+++ src/main/java/de/nexus/springbootvue/security/filters/StaticKeyAuthenticationFilter.java
@@ -0,0 +1,30 @@
+package de.nexus.springbootvue.security.filters;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class StaticKeyAuthenticationFilter implements Filter {
+//    @Value("${authorization.key}")
+//    private String authorizationKey;
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+//        String authHeader = request.getHeader("Authorization");
+//        if (authorizationKey.equals(authHeader)) {
+//            filterChain.doFilter(request, response);
+//        } else {
+//            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
