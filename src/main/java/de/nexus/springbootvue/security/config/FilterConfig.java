23a0fa20d9d2cccf859ed7d94c78004b40a2776b
valeryoster <oster_valera@hotmail.com>
2025-05-08 13:46:03 +0200

    spring security

--- src/main/java/de/nexus/springbootvue/security/config/FilterConfig.java
+++ src/main/java/de/nexus/springbootvue/security/config/FilterConfig.java
@@ -1,12 +1,9 @@
 package de.nexus.springbootvue.security.config;
 
-import de.nexus.springbootvue.security.filters.RequestValidationFilter;
-import de.nexus.springbootvue.security.filters.StaticKeyAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class FilterConfig {
@@ -17,7 +14,8 @@ public class FilterConfig {
                 .authorizeHttpRequests(authorizeRequests -> {
                     authorizeRequests.requestMatchers("/hello").authenticated()
                             .anyRequest().permitAll()
-                            ;});
+                    ;
+                });
         return http.build();
     }
 }
