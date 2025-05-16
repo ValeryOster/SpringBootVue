a2044c328e0c03f0ffe98d6da9164e9c3e592cf2
valeryoster <oster_valera@hotmail.com>
2025-05-16 12:27:49 +0200

    vue.js

---
+++ src/frontend/src/compfromvideo/TeilTree.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <h3 @click="showAlert">{{ this.message }}</h3>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World',
+
+    }
+  },
+  methods:{
+    showAlert() {
+      let title = 'Teil Drei'
+      this.message = 'Dies ist der dritte Teil des Tutorials.' + title;
+      title = "auch";
+    },
+    reactive(obj) {
+      return new Proxy(obj, {});
+    }
+  },
+  beforeCreate() {
+    console.log('beforeCreate');
+  },
+  created() {
+    console.log('created');
+  },
+  beforeMount() {
+    console.log('beforeMount');
+  },
+  beforeUpdate() {
+    console.log('beforeUpdate');
+  },
+  updated() {
+    console.log('updated');
+  },
+  mounted() {
+    console.log('mounted');
+  },
+  beforeUnmount() {
+    console.log('unmounted');
+  }
+
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
