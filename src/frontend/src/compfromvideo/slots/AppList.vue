77624a4e7af66e275f07f923cad0e695ff17d0b9
valeryoster <oster_valera@hotmail.com>
2025-05-22 23:33:14 +0200

    vue.js

---
+++ src/frontend/src/compfromvideo/slots/AppList.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div class="card">
+    <ul>
+      <li v-for="(item,index) in 5" :key="index">
+        <slot :iter="item" :idx="index"></slot>
+      </li>
+
+    </ul>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/frontend/src/compfromvideo/slots/SlotTest.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container pt-1">
+    <div class="card">
+      <h2>Slots</h2>
+    </div>
+    <app-list>
+      <template #default="{idx, iter}">
+        <span style="color: #c25205">
+          {{idx +1 }} :
+          {{iter}}
+        </span>
+      </template>
+    </app-list>
+
+    <app-block>
+      <template v-slot:header>
+        <h2>Mein Titel</h2>
+      </template>
+      <template #default>
+        <p>mein Text</p>
+      </template>
+      <template #footer>
+        <hr/>
+        <small>The Footer</small>
+      </template>
+    </app-block>
+  </div>
+</template>
+
+<script>
+import AppBlock from './AppBlock.vue'
+import AppList from './AppList.vue'
+
+export default {
+  components: {AppBlock, AppList},
+}
+</script>
+
+<style>
+</style>
