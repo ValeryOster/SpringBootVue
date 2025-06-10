14a036d050d9051c7bcef58a3ed4b0bb02ae0fc9
valeryoster <oster_valera@hotmail.com>
2025-06-10 18:13:15 +0200

    frontend: form

---
+++ src/frontend/src/components/AppAlertvue.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="alert" :class="type">
+    <h3>{{ title }}</h3>
+    <p>{{ text }}</p>
+    <button class="btn" :class="type" v-if="closable" @click="$emit('close')">Close</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['close'],
+  props: {
+    text: String,
+    title: String,
+    closable: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'primary',
+      validator(value) {
+        return ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'].includes(value);
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
