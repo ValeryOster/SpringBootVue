a8cd973cc380321f25a3b442aa1a71b557910f59
valeryoster <oster_valera@hotmail.com>
2025-06-11 22:15:47 +0200

    frontend: form

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,45 +1,38 @@
 <template>
   <div class="container">
-    <form class="card" @submit.prevent="createPerson">
-      <h2>DateBase anbinden</h2>
-      <div class="form-control">
-        <label for="name">Name</label>
-        <input type="text" id="name" placeholder="Name" v-model="person.name" />
-      </div>
-      <button class="btn primary" :disabled="person.name.length === 0">Submit</button>
-    </form>
+    <app-alertvue
+        v-if="alert"
+        :text="'Dies ist eine Warnung!'"
+        :title="'Achtung'"
+        :closable="true"
+        type="danger"
+        @close="alert = false"
+    ></app-alertvue>
+    <div class="card">
+      <button class="btn primary" @click="alert=true">Alle Nachrichten</button>
+    </div>
 
   </div>
 </template>
-<script>
 
-import axios from "axios";
+<script>
+import AppAlertvue from "@/components/AppAlertvue.vue";
 
 export default {
   data() {
     return {
-      persons: [],
-      person:{
-        id: null,
-        name: '',
-        email: '',
-        phone: '',
-      },
+      alert: false,
     }
   },
+  components: {AppAlertvue},
   methods: {
-    async createPerson() {
-      console.log(this.person);
-      axios.post('http://localhost:8085/api/person/save', this.person).then(response => {
-        console.log(response.data);
-        this.persons.push(response.data);
-      }).catch(error => {
-        console.error("Error creating person:", error);
-      })
-    }
-  },
+    changeColor() {
+      this.color = 'danger';
+    },
+  }
 }
 </script>
 
 <style>
+
 </style>
