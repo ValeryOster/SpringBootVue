e5bed28e27748d376b0c10563f869e025584a891
valeryoster <oster_valera@hotmail.com>
2025-06-20 22:55:51 +0200

    spring security

--- src/main/java/de/nexus/springbootvue/security/config/ProjectConfig.java
+++ src/main/java/de/nexus/springbootvue/security/config/ProjectConfig.java
@@ -2,6 +2,13 @@ package de.nexus.springbootvue.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import javax.sql.DataSource;
 import java.util.HashMap;
@@ -9,17 +16,17 @@ import java.util.Map;
 
 @Configuration
 public class ProjectConfig {
-//    @Bean
-//    public UserDetailsService userDetailsService(DataSource dataSource) {
-//        String usersByUsernameQuery =
-//                "select username, password, enabled from users where username = ?";
-//        String authsByUserQuery =
-//                "select username, authority from spring.authorities where username = ?";
-//        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
-//        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
-//        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
-//        return userDetailsManager;
-//    }
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery =
+                "select username, password, enabled from users where username = ?";
+        String authsByUserQuery =
+                "select username, authority from spring.authorities where username = ?";
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
+    }
 //
 //    @Bean
 //    public UserDetailsService userDetailsService(DataSource dataSource) {
@@ -30,12 +37,12 @@ public class ProjectConfig {
 //    public PasswordEncoder passwordEncoder() {
 //        return NoOpPasswordEncoder.getInstance();
 //    }
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        Map<String, PasswordEncoder> encoders = new HashMap<>();
-//        encoders.put("noop", NoOpPasswordEncoder.getInstance());
-//        encoders.put("bcrypt", new BCryptPasswordEncoder());
-//        encoders.put("scrypt",  SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8());
-//        return new DelegatingPasswordEncoder("bcrypt", encoders);
-//    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put("scrypt",  SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8());
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
+    }
 }
