2535c35d5b88940bfd09e790825621b36bd80ec1
valeryoster <oster_valera@hotmail.com>
2025-06-29 22:00:13 +0200

    spring security

--- src/main/java/de/nexus/springbootvue/security/config/ProjectConfig.java
+++ src/main/java/de/nexus/springbootvue/security/config/ProjectConfig.java
@@ -16,33 +16,33 @@ import java.util.Map;
 
 @Configuration
 public class ProjectConfig {
-    @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource) {
-        String usersByUsernameQuery =
-                "select username, password, enabled from users where username = ?";
-        String authsByUserQuery =
-                "select username, authority from spring.authorities where username = ?";
-        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
-        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
-        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
-        return userDetailsManager;
-    }
-//
 //    @Bean
 //    public UserDetailsService userDetailsService(DataSource dataSource) {
-//        return new JdbcUserDetailsManager(dataSource);
+//        String usersByUsernameQuery =
+//                "select username, password, enabled from users where username = ?";
+//        String authsByUserQuery =
+//                "select username, authority from spring.authorities where username = ?";
+//        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+//        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+//        return userDetailsManager;
 //    }
 //
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
 //        @Bean
 //    public PasswordEncoder passwordEncoder() {
 //        return NoOpPasswordEncoder.getInstance();
 //    }
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        Map<String, PasswordEncoder> encoders = new HashMap<>();
-        encoders.put("noop", NoOpPasswordEncoder.getInstance());
-        encoders.put("bcrypt", new BCryptPasswordEncoder());
-        encoders.put("scrypt",  SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8());
-        return new DelegatingPasswordEncoder("bcrypt", encoders);
-    }
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        Map<String, PasswordEncoder> encoders = new HashMap<>();
+//        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+//        encoders.put("bcrypt", new BCryptPasswordEncoder());
+//        encoders.put("scrypt",  SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8());
+//        return new DelegatingPasswordEncoder("bcrypt", encoders);
+//    }
 }
